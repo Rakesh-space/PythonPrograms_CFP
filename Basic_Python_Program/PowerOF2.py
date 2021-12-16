45e6f86f10be52abd0d860d508b60676fef47695
unknown <rakeshsmusale03@gmail.com>
2021-12-16 22:01:34 +0530

    [Rakesh] Added:Power Of 2.

---
+++ Basic_Python_Program/PowerOF2.py
@@ -0,0 +1,11 @@
+'''
+Author: Rakesh Musale
+Date: 2022-15-12 02:40:12
+Last Modified by: Rakesh Musale
+Last Modified time: 2022-15-12 2.30:00:15
+Title : Find out Power of 2
+'''
+
+for num in range(1,31) :
+    print("\n Table of power of 2 is:",2*num)
+    num +=num
